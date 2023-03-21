b973d7af6f727f730bf2265740fcf149ce64d18c
KaterinaPilipets <ket.pil.glass@gmail.com>
2023-03-21 20:58:13 +0200

    fix

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -6,31 +6,38 @@ import { List } from './ImageGallery.styled';
 import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
 import { Modal } from 'components/Modal/Modal';
-export const ImageGallery = searchValue => {
-  console.log(searchValue);
+export const ImageGallery = ({ searchValue }) => {
   const [images, setImages] = useState([]);
   const [status, setStatus] = useState('idle');
   const [page, setPage] = useState(1);
   const [maxPage, setMaxPage] = useState(1);
   const [showModal, setShowModal] = useState(false);
   const [modalImg, setModalImg] = useState('');
+  const [value, setValue] = useState('');
   useEffect(() => {
-    setStatus('idle');
-    setPage(1);
+    if (searchValue) {
+      setValue(searchValue);
+      setMaxPage(1);
+      setImages([]);
+      setStatus('idle');
+      setPage(1);
+    }
   }, [searchValue]);
   useEffect(() => {
-    setStatus('pending');
-    fetchGallery(searchValue, page)
-      .then(resp => {
-        setMaxPage(resp.totalHits / PER_PAGE);
-        setStatus('resolved');
-        setImages(prevState => [...prevState, ...resp.hits]);
-      })
-      .catch(error => {
-        console.log('error :>> ', error);
-        setStatus('rejected');
-      });
-  }, [searchValue, page]);
+    if (value) {
+      setStatus('pending');
+      fetchGallery(value, page)
+        .then(resp => {
+          setMaxPage(resp.totalHits / PER_PAGE);
+          setStatus('resolved');
+          setImages(prevState => [...prevState, ...resp.hits]);
+        })
+        .catch(error => {
+          console.log('error :>> ', error);
+          setStatus('rejected');
+        });
+    }
+  }, [value, page]);
 
   const loadNextPage = () => {
     setPage(prevState => prevState + 1);
@@ -60,7 +67,9 @@ export const ImageGallery = searchValue => {
         ))}
       </List>
       {status === 'pending' && <Loader />}
-      {page < maxPage && <Button onClick={loadNextPage}>Load more</Button>}
+      {page < maxPage && status !== 'pending' && (
+        <Button onClick={loadNextPage}>Load more</Button>
+      )}
     </div>
   );
   // }
